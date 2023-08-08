1f0da8ab3404c3c2b2501c622fc9d8a8f7288d4f
dev-1113 <jurnt2000@yahoo.com>
2023-08-08 12:04:56 +0100

    see if this works...

---
+++ nav.js
@@ -0,0 +1,17 @@
+document.write('<nav class="navbar navbar-inverse">\
+<div class="container-fluid">\
+  <ul class="nav navbar-nav">\
+    <li class="active"><a href="https://dev-1113.github.io/merlin/index.html">Home</a></li>\
+    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">Front-End Mentor<span class="caret"></span></a>\
+      <ul class="dropdown-menu">\
+        <li><a href="https://dev-1113.github.io/merlin/results-summary/index.html">Results Summary</a></li>\
+        <li><a href="https://dev-1113.github.io/merlin/product/index.html">Product Preview</a></li>\
+        <li><a href="https://dev-1113.github.io/merlin/interactive-rating-component-main/index.html">Interactive Rating</a></li>\
+      </ul>\
+    </li>\
+    <li><a href="#">UNI Project</a></li>\
+    <li><a href="#">Taxi Sharing App</a></li>\
+  </ul>\
+</div>\
+</nav>\
+');
