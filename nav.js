738954de8559a459c5b0d0e63d5dd6e2be9891f4
dev-1113 <jurnt2000@yahoo.com>
2023-08-09 11:28:43 +0100

    Added four more component projects
    1) Changed the links in the nav.js
    2) Added four projects

--- nav.js
+++ nav.js
@@ -4,9 +4,12 @@ document.write('<nav class="navbar navbar-inverse">\
     <li class="active"><a href="https://dev-1113.github.io/merlin/index.html">Home</a></li>\
     <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">Front-End Mentor<span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="https://dev-1113.github.io/merlin/results-summary/index.html">Results Summary</a></li>\
         <li><a href="https://dev-1113.github.io/merlin/product/index.html">Product Preview</a></li>\
         <li><a href="https://dev-1113.github.io/merlin/interactive-rating-component-main/index.html">Interactive Rating</a></li>\
+        <li><a href="https://dev-1113.github.io/merlin/qr-code-component-main/index.html">QR Component</a></li>\
+        <li><a href="https://dev-1113.github.io/merlin/nft-preview-card-component-main/index.html">Preview Card</a></li>\
+        <li><a href="https://dev-1113.github.io/merlin/order-summary-component-main/index.html">Order Summary</a></li>\
+        <li><a href="https://dev-1113.github.io/merlin/3-column-preview-card-component-main/index.html">3 Column Card</a></li>\
       </ul>\
     </li>\
     <li><a href="#">UNI Project</a></li>\
