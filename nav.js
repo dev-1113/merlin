ba2256639b6784929ef0b5babbe3428b0db0af6b
dev-1113 <jurnt2000@yahoo.com>
2023-08-09 10:54:10 +0100

    Update nav.js
    Changed the a href url to local

--- nav.js
+++ nav.js
@@ -4,9 +4,9 @@ document.write('<nav class="navbar navbar-inverse">\
     <li class="active"><a href="https://dev-1113.github.io/merlin/index.html">Home</a></li>\
     <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">Front-End Mentor<span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="https://dev-1113.github.io/merlin/results-summary/index.html">Results Summary</a></li>\
-        <li><a href="https://dev-1113.github.io/merlin/product/index.html">Product Preview</a></li>\
-        <li><a href="https://dev-1113.github.io/merlin/interactive-rating-component-main/index.html">Interactive Rating</a></li>\
+        <li><a href="results-summary/index.html">Results Summary</a></li>\
+        <li><a href="product/index.html">Product Preview</a></li>\
+        <li><a href="interactive-rating-component-main/index.html">Interactive Rating</a></li>\
       </ul>\
     </li>\
     <li><a href="#">UNI Project</a></li>\
